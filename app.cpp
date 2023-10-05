245c6f2d0588f7dc5b27bee097d7deef5de52113
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-10-05 16:04:42 +0300

    ✔️ first commit

---
+++ app.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+  std::vector<int> items = {1, 2, 3};
+  std::cout << "i am here !!!!" << std::endl;
+  return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+  std::cout << "hello, mu name is yousef" << std::endl;
+  return 0;
+}
+
