d5b8992c343f7a51a0e9fcb313c378e7ce4742a0
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-10-07 23:59:10 +0300

    ✔️ first commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-  std::cout << "Line 1 \n";
-  std::cout << "Line 2 \n";
-  std::cout << "Line 3 \n";
+  int price = 200;
+  int a;
+  cout << "the price is " << price << "\n";
+  cout << "Line 1 \n";
+  cout << "Line 2 \n";
+  cout << "Line 3 \n";
+  cout << a;
   return 0;
 }
 
