931898e1e7e4b3ea2e89b753cb4b9a90daf31191
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-10-08 15:18:42 +0300

    ✔️ first commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 int main(){
-  int age;
-  cin >> age;
-  int age_In_Years = age * 365;
-  int age_In_Hours = age_In_Years * 24;
-  cout << "the age in years is " << age_In_Years << "years\n";
-  cout << "the age in hours is " << age_In_Hours << "hours\n";
+  cout << "Mr Yousef Elgohary \\";
   return 0;
 }
