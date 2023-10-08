48fcf231a945091e2db966d40b665b4d2bea7141
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-10-08 14:49:21 +0300

    ✔️ first commit

--- main.cpp
+++ main.cpp
@@ -4,14 +4,9 @@ using namespace std;
 int main(){
   int age;
   cin >> age;
-  cout << age;
+  int age_In_Years = age * 365;
+  int age_In_Hours = age_In_Years * 24;
+  cout << "the age in years is " << age_In_Years << "years\n";
+  cout << "the age in hours is " << age_In_Hours << "hours\n";
   return 0;
 }
-
-
-/* 
-  this is a multi line comment
-  this is a multi line comment
-  this is a multi line comment
-*/
-// this is a single line comment
