dbd952fbcd43071bedf183e709ef5a1e65c11073
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-10-08 14:30:56 +0300

    ✔️ first commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main(){
-  int price = 200;
-  int a;
-  cout << "the price is " << price << "\n";
-  cout << "Line 1 \n";
-  cout << "Line 2 \n";
-  cout << "Line 3 \n";
-  cout << a;
+  int age;
+  cin >> age;
+  cout << age;
   return 0;
 }
 
